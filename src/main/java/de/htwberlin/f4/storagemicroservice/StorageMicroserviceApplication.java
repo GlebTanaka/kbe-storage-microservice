8cbe6fbee5ee3ebe417c36fbf1afa78fbfdd8e27
GlebTanaka <gleb5655@gmail.com>
2021-12-04 15:59:25 +0100

    first commit

---
+++ src/main/java/de/htwberlin/f4/storagemicroservice/StorageMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.f4.storagemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/f4/storagemicroservice/StorageMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.f4.storagemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
